61983cb02557ff1ee3517a447c83b648ce03a5f8
wgrape <lvsi@changba.com>
2021-08-28 19:02:35 +0800

    增加sql语言的扩展-测试版

--- lang/sql-define.js
+++ lang/sql-define.js
@@ -143,12 +143,11 @@ const CHARSET_CONST = {
 
     // 关键字
     KEYWORD: [
-        'char', 'int', 'short', 'long', 'float', 'double', 'sizeof', 'signed', 'unsigned',
-        'if', 'else', 'while', 'for', 'do', 'break', 'continue', 'goto',
-        'void', 'return', 'switch', 'case', 'default',
-        'const', 'static', 'auto', 'extern', 'register',
-        'struct', 'union', 'enum', 'typedef',
-        'include',
+        'select', 'from', 'where', 'and', 'group', 'by', 'order', 'desc', 'asc', 'limit',
+        'add', 'constraint', 'alter', 'column', 'as', 'database', 'between', 'case', 'check',
+        'create', 'alter', 'table', 'unique', 'index',
+        'delete', 'distinct', 'drop', 'full', 'join', 'outer', 'inner', 'having', 'in', 'or', 'null',
+        'insert', 'update', 'set', 'values', 'like', 'not', 'primary', 'into', 'top', 'union', 'all', 'view',
     ],
 };
 
@@ -326,133 +325,7 @@ let tool = {
 // 单元测试
 let unitTest = {
     returnCaseList() {
-        return [
-            {
-                "input": "",
-                "output": 0,
-            },
-            {
-                "input": "int",
-                "output": [{"type": "Keyword", "value": "int"}],
-            },
-            {
-                "input": "int;",
-                "output": [
-                    {"type": "Keyword", "value": "int"},
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": "int a",
-                "output": [
-                    {"type": "Keyword", "value": "int"},
-                    {"type": "Identifier", "value": "a"},
-                ],
-            },
-            {
-                "input": "int a;",
-                "output": [
-                    {"type": "Keyword", "value": "int"},
-                    {"type": "Identifier", "value": "a"},
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": ";",
-                "output": [
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": ";int",
-                "output": [
-                    {"type": "Symbol", "value": ";"},
-                    {"type": "Keyword", "value": "int"},
-                ],
-            },
-            {
-                "input": ";int;",
-                "output": [
-                    {"type": "Symbol", "value": ";"},
-                    {"type": "Keyword", "value": "int"},
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": "char;",
-                "output": [
-                    {"type": "Keyword", "value": "char"},
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": "float;",
-                "output": [
-                    {"type": "Keyword", "value": "float"},
-                    {"type": "Symbol", "value": ";"},
-                ],
-            },
-            {
-                "input": "return 0.2224322432;",
-                "output": 3,
-            },
-            {
-                "input": ";;;;;",
-                "output": 5,
-            },
-            {
-                "input": 'int a = b^2;int a = b~2;',
-                "output": 14,
-            },
-            {
-                "input": "#include <stdio.h>",
-                "output": 7,
-            },
-            {
-                "input": "char *str = \"This is a string.\";",
-                "output": 6,
-            },
-            {
-                "input": "printf(\"char: %c\\n\", c);",
-                "output": 7,
-            },
-            {
-                "input": "printf(\"string1: %s\\n\", str);",
-                "output": 7,
-            },
-            {
-                "input":"signed int a = 23;unsigned int b = 24324;",
-                "output": 12,
-            },
-            {
-                "input": "'A\"'';rt;ewr';trewl;\"'t,w4;5lmyktr ;jwngh25t;j1",
-                "output": 12,
-            },
-            {
-                "input": "%E^SP&*@#$hbj89hr24576rtgvyx6^%^$S%%32",
-                "output": 16,
-            },
-            {
-                "input": "[][<>[]]][][[[][[]{]{}[}",
-                "output": 24,
-            },
-            {
-                "input": "fruit = apples + oranges;",
-                "output": 6,
-            },
-            {
-                "input": "/* Hello,World */",
-                "output": 7,
-            },
-            {
-                "input": "''\"\">><><>KL:<L:L:M><>>>>><<!!!=!===!<<>'';'\n",
-                "output": 32,
-            },
-            {
-                "input": "~^%&541$!#$!t¥r54.,l;.',l\"/[?\\]\"[\\o''k\"[21'''\\\\''][kop",
-                "output": 24,
-            }
-        ];
+        return [];
     },
 };
 
