385c8ff72334b8217b5a58bed12a380b4db73df9
wgrape <lvsi@changba.com>
2021-08-28 20:23:03 +0800

    添加SQL单元测试

--- lang/sql-define.js
+++ lang/sql-define.js
@@ -325,7 +325,36 @@ let tool = {
 // 单元测试
 let unitTest = {
     returnCaseList() {
-        return [];
+        return [
+            {
+                "input": "",
+                "output": 0,
+            },
+            {
+                "input": "select",
+                "output": 1,
+            },
+            {
+                "input": "select*",
+                "output": 2,
+            },
+            {
+                "input": "create table;",
+                "output": 3,
+            },
+            {
+                "input": "select id,name,student.age from student where name = \"张三\" and score=23.22; ",
+                "output": 19,
+            },
+            {
+                "input": "select id,name,student.age from student where (id<22) and (id>2) group by name having id > 20 order by id desc limit 0,10;",
+                "output": 38,
+            },
+            {
+                "input": "select * from table where id >= 2 and id != 2 or id == 2;",
+                "output": 17,
+            },
+        ];
     },
 };
 
